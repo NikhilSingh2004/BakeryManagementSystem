6b8ab8c5e4ad428763e809232e1a5acb5699eddd
Nikhil Singh <nikhilsinghcr007@gmail.com>
2024-01-08 11:46:49 +0530

    Initialization Setup

--- BakeryMSys/core/forms.py
+++ BakeryMSys/core/forms.py
@@ -12,6 +12,17 @@ class UpdateOrder(forms.Form):
     order_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control' , 'placeholder': 'Order Name'}))
     order_quantity = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class': 'form-control' , 'placeholder': 'Quantity'}))
 
+    def __init__(self, *args, **kwargs):
+        instance = kwargs.pop('instance', None)
+
+        super(UpdateOrder, self).__init__(*args, **kwargs)
+
+        if instance:
+            self.fields['order_id'].initial = instance.order_id
+            self.fields['customer_name'].initial = instance.customer_name
+            self.fields['order_name'].initial = instance.order_name
+            self.fields['order_quantity'].initial = instance.order_quantity
+
 class FindOrder(forms.Form):
     order_id = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class':'form-control' , 'placeholder': 'Order ID'}))
 
